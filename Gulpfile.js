24009f67064d0d1db040081b3a91380e2e3400ac
Julio Barboza <julio.barboza@possible.com>
2017-09-19 22:01:57 -0600

    adding AMP boilerplate wit SASS, Gulp and BrowserSync

---
+++ Gulpfile.js
@@ -0,0 +1,51 @@
+// Import Gulp and Plumber:
+var gulp = require('gulp');
+var plumber = require('gulp-plumber'); // Plumber will make sure if Gulp is not crashing when CSS compilation prints errors.
+
+// SASS compilation task:
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+gulp.task('sass', function () {
+    gulp.src('./src/sass/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('./dist'));
+});
+
+// HTML generation task
+var fs = require("fs");
+var inject = require('gulp-inject-string');
+gulp.task('html', function () {
+    var cssContent = fs.readFileSync("./dist/main.css", "utf8");
+    gulp.src("./src/html/*.html")
+        .pipe(inject.after('style amp-custom>', cssContent))
+        .pipe(gulp.dest("./dist"))
+        .pipe(reload({
+            stream: true
+    }));
+});
+
+// BrowserSync serve task
+var browser = require('browser-sync');
+var reload = browser.reload;
+gulp.task('serve', function() {
+    browser({
+        port: 4500,
+        open: false,
+        ghostMode: false,
+        server: {
+            baseDir: './dist'
+        }
+    });
+});
+
+// Watch task
+gulp.task('watch', function() {
+    gulp.watch("./src/sass/**", ['sass']);
+    gulp.watch("./dist/*.css", ['html']);
+    gulp.watch("./src/html/*.html", ['html']);
+});
+
+// Default task
+gulp.task('default', ['sass', 'html', 'watch', 'serve']);
